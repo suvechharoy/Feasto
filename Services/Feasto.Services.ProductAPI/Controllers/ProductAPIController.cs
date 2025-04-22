2db53f1a6812d47131c6a36e91360306370b555d
Suvechha Roy <suvechha.roy@thoughtworks.com>
2025-04-23 00:05:25 +0530

    Deploying services on azure

--- Services/Feasto.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Services/Feasto.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -64,23 +64,31 @@ public class ProductAPIController : ControllerBase
 
             if (productDTO.Image != null)
             {
-                string fileName = product.ProductId + Path.GetExtension(productDTO.Image.FileName); //generate a new filename based on productID and preserve the extension. So its extracting the extension and appending it to productID
-                //string filePath = @"wwwroot\ProductImages\" + fileName; 
-                string uploadPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "ProductImages"); //complete file path 
+                string fileName = product.ProductId + Path.GetExtension(productDTO.Image.FileName);
+                string filePath = @"ProductImages\" + fileName;
+
+                // Construct the absolute path within the Azure App Service wwwroot for saving
+                string uploadPath = Path.Combine(@"C:\home\site\wwwroot\", "ProductImages");
                 if (!Directory.Exists(uploadPath))
                 {
                     Directory.CreateDirectory(uploadPath);
                 }
-                string filePathDirectory = Path.Combine(uploadPath, fileName); //get the file path directory by combining the file path with current directory. This will give complete location to wwwroot folder
-                using (var fileStream = new FileStream(filePathDirectory, FileMode.Create)) //copy the image to the folder fetched above i.e., filePathDirectory
+                string filePathDirectory = Path.Combine(uploadPath, fileName);
+                
+                // Delete any existing image with the same name (using the Azure path)
+                FileInfo file = new FileInfo(filePathDirectory);
+                if (file.Exists)
+                {
+                    file.Delete();
+                }
+                
+                using (var fileStream = new FileStream(filePathDirectory, FileMode.Create))
                 {
                     productDTO.Image.CopyTo(fileStream);
                 }
-
-                var baseUrl =
-                    $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}"; 
+                var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
                 product.ImageUrl = baseUrl+ "/ProductImages/"+ fileName;
-                product.ImageLocalPath = filePathDirectory;
+                product.ImageLocalPath = filePath;
             }
             else
             {
